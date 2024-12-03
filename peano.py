6428a712302625126f815a39ea9614ac32701e7e
gor <gorparsyan13@mail.ru>
2024-12-03 14:30:38 +0400

    Create Project and Peano numbers

---
+++ peano.py
@@ -0,0 +1,26 @@
+def sequent(number):
+    seq_number = number[0:len(number)]
+    seq_number.append("յաջորդ")
+    return seq_number
+
+
+zero = ["զրոյ"]
+
+one = sequent(zero)
+
+two = sequent(one)
+
+three = sequent(two)
+
+four = sequent(three)
+
+five = sequent(four)
+
+six = sequent(five)
+
+seven = sequent(six)
+
+eight = sequent(seven)
+
+nine = sequent(eight)
+
